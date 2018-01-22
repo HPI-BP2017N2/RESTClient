fae03f8d1c6a8c4c8800eb5bc7cbf893a592d9e3
deeps96 <leonardo.huebscher.business@gmail.com>
2018-01-22 18:33:10 +0100

    Added RuleEntry-Pojo and edited Offer to work with ShopRulesGenerator. #3

---
+++ src/main/java/de/hpi/restclient/pojo/RuleEntry.java
@@ -0,0 +1,12 @@
+package de.hpi.restclient.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RuleEntry {
+
+    private boolean resultAsPlainText;
+    private String xPath, attribute;
+
+}
