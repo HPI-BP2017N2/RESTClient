684bd648161c1210d281d19d371a10a829dcbd76
dey4ss <lindnerdaniel@yahoo.de>
2018-01-30 18:14:25 +0100

    added pojo for Matching

---
+++ src/main/java/de/hpi/restclient/dto/ParsedOffer.java
@@ -0,0 +1,11 @@
+package de.hpi.restclient.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+
+public class ParsedOffer {
+    @Getter(AccessLevel.PUBLIC) private long shopId;
+    @Getter(AccessLevel.PUBLIC) private String offerTitle, ean, han, sku, url, categoryString;
+    @Getter(AccessLevel.PUBLIC) private double price;
+}
