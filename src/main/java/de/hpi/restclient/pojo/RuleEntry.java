895e0b28d7dc8d0f2474097e10f2d3e05beda21e
deeps96 <leonardo.huebscher.business@gmail.com>
2018-01-30 11:30:40 +0100

    Created constructor for RuleEntry-POJO required by ShopRulesGenerator.

--- src/main/java/de/hpi/restclient/pojo/RuleEntry.java
+++ src/main/java/de/hpi/restclient/pojo/RuleEntry.java
@@ -9,4 +9,12 @@ public class RuleEntry {
     private boolean resultAsPlainText;
     private String attribute, selector;
 
+    public RuleEntry() {
+        setResultAsPlainText(true);
+    }
+
+    public RuleEntry(String selector) {
+        this();
+        setSelector(selector);
+    }
 }
