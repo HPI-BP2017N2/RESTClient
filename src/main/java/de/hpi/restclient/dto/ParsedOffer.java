cdd3c9c6b3a19da15370844ed20e3fffd5b2d039
dey4ss <lindnerdaniel@yahoo.de>
2018-01-31 11:32:43 +0100

    transferred MatchingResponse dto to RESTCLient

--- src/main/java/de/hpi/restclient/dto/ParsedOffer.java
+++ src/main/java/de/hpi/restclient/dto/ParsedOffer.java
@@ -1,11 +1,11 @@
 package de.hpi.restclient.dto;
 
-import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter @Setter
 public class ParsedOffer {
-    @Getter(AccessLevel.PUBLIC) private long shopId;
-    @Getter(AccessLevel.PUBLIC) private String offerTitle, ean, han, sku, url, categoryString;
-    @Getter(AccessLevel.PUBLIC) private double price;
+    private long shopId;
+    private String offerTitle, ean, han, sku, url, categoryString;
+    private double price;
 }
