87f4ee8bf20e93d870913d94c37612f569f0d2f1
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-01 11:27:00 +0100

    Added dto´s for URLCleaner. #2

---
+++ src/main/java/de/hpi/restclient/dto/CleanURLParameter.java
@@ -0,0 +1,12 @@
+package de.hpi.restclient.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class CleanURLParameter {
+
+    private String url;
+}
