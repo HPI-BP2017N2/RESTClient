d3adbd2add6559db9da1540b494f039660f78541
Jonas Pohlmann <jonaspohlmann@yahoo.de>
2018-02-06 11:45:00 +0100

    Changed parameter for URLCleaner

--- src/main/java/de/hpi/restclient/properties/URLCleanerProperties.java
+++ src/main/java/de/hpi/restclient/properties/URLCleanerProperties.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @EnableConfigurationProperties
-@ConfigurationProperties("bpbridge")
+@ConfigurationProperties("urlcleaner")
 @Getter @Setter
 public class URLCleanerProperties {
 
