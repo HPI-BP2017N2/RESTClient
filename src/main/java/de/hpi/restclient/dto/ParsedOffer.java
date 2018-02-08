4572e3fd343c6f9a2034a5a8c8db555d29a9e5e2
dey4ss <lindnerdaniel@yahoo.de>
2018-02-08 17:28:43 +0100

    added brand attribute to ParsedOffer dto

--- src/main/java/de/hpi/restclient/dto/ParsedOffer.java
+++ src/main/java/de/hpi/restclient/dto/ParsedOffer.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter @Setter
 public class ParsedOffer {
     private long shopId;
-    private String offerTitle, ean, han, sku, url, categoryString;
+    private String offerTitle, ean, han, sku, url, categoryString, brand;
     private double price;
 }
