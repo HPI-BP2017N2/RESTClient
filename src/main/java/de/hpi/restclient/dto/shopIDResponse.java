63a441d6d9b28f4b7489e489b8f463f669853335
Stanislav Nowogrudski <dj.steam@gmail.com>
2018-02-08 20:53:10 +0100

    Update shopIDResponse.java
    review

--- src/main/java/de/hpi/restclient/dto/shopIDResponse.java
+++ src/main/java/de/hpi/restclient/dto/shopIDResponse.java
@@ -2,7 +2,7 @@ package de.hpi.restclient.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-
+// TODO - java class names must start with a capital letter
 @Getter @Setter
 public class shopIDResponse {
     String shopUrl;
