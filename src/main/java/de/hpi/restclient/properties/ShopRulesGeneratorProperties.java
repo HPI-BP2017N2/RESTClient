3a33dc9ed085920c3910586ac4f9ac453fcf33ac
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-08 11:56:15 +0100

    Created properties for #3.

---
+++ src/main/java/de/hpi/restclient/properties/ShopRulesGeneratorProperties.java
@@ -0,0 +1,19 @@
+package de.hpi.restclient.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableConfigurationProperties
+@ConfigurationProperties("shoprulesgenerator")
+@Getter @Setter
+public class ShopRulesGeneratorProperties {
+
+    @NotBlank
+    private String root, getRulesRoute;
+
+}
