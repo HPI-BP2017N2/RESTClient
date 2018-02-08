ba9c9fc60c8a9864832da1b207cb98eb7efd92ab
Stanislav Nowogrudski <dj.steam@gmail.com>
2018-02-08 20:52:32 +0100

    Update restCrawlerConfig.java
    review

--- src/main/java/de/hpi/restclient/dto/restCrawlerConfig.java
+++ src/main/java/de/hpi/restclient/dto/restCrawlerConfig.java
@@ -1,7 +1,7 @@
 package de.hpi.restclient.dto;
 
 import lombok.Getter;
-
+// TODO - java class names must start with a capital letter
 public class restCrawlerConfig {
 
     @Getter private Long shopID;
