7e7b69349f44f54ad120ae3a4be0143175694d35
dey4ss <lindnerdaniel@yahoo.de>
2018-02-19 15:19:17 +0100

    added JSONCreator to some dtos

--- src/main/java/de/hpi/restclient/dto/CrawledPage.java
+++ src/main/java/de/hpi/restclient/dto/CrawledPage.java
@@ -1,12 +1,29 @@
 package de.hpi.restclient.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter @Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CrawledPage {
-    long shopID;
-    long timestamp;
-    String url;
-    String htmlSource;
+    private long shopID;
+    private long timestamp;
+    private String url;
+    private String htmlSource;
+
+    @JsonCreator
+    public CrawledPage(@JsonProperty("shopId") long shopId,
+                       @JsonProperty("timestamp") long timestamp,
+                       @JsonProperty("url") String url,
+                       @JsonProperty("htmlSource") String htmlSource){
+        setShopID(shopId);
+        setTimestamp(timestamp);
+        setUrl(url);
+        setHtmlSource(htmlSource);
+    }
 }
